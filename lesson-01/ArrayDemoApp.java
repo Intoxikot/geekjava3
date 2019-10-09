0874d6a9b70703fce7ae298bb8dae9d91053e2eb
koteev <netoxikot@gmail.com>
2019-10-09 06:54:57 +0500

    Добавлен метод, который меняет два элемента массива местами

--- lesson-01/ArrayDemoApp.java
+++ lesson-01/ArrayDemoApp.java
@@ -5,10 +5,29 @@ class Array<T> {
     Array(T[] data) {
         this.data = data;
     }
+
+    // Метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
+    void replace(int first, int second) {
+        T temp = data[first];
+        data[first] = data[second];
+        data[second] = temp;
+    }
+
+    void show() {
+        System.out.print("array: ");
+        for (int i = 0; i < data.length; i++)
+            System.out.print(data[i] + " ");
+        System.out.println();
+    }
+
 }
 
 public class ArrayDemoApp {
     public static void main(String[] args) {
+        // Проверим замену двух элементов массива по индексу
         Array<Integer> box = new Array<>(new Integer[]{1,2,3,4,5});
+        box.show();
+        box.replace(2,4);
+        box.show();
     }
 }
