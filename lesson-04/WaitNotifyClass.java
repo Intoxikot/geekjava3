bbdbe17188498890e544fb2286ff023e547907b9
koteev <netoxikot@gmail.com>
2019-10-21 12:14:58 +0500

    Исходные установки задачи: последовательно выполняются потоки ABABAB

---
+++ lesson-04/WaitNotifyClass.java
@@ -0,0 +1,51 @@
+
+public class WaitNotifyClass {
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+        WaitNotifyClass w = new WaitNotifyClass();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(() -> {
+            w.printB();
+        });
+        t1.start();
+        t2.start();
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 3; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'A';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
